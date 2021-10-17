9a90598c9d850e9604ff18e13b0032f1b8170a65
Flip <aungkhantpaing6789@gmail.com>
2021-10-17 20:05:17 +0630

    commit requirements

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ cogs = [music]
 client = commands.Bot(command_prefix='\\')
 client.remove_command('help')
 for i in range(len(cogs)):
-  cogs[i].setup(client)
+    cogs[i].setup(client)
 
 client.run("ODk3ODQ5OTM0NDk2Mjc2NTAx.YWbp6Q.RSS69jEftslD1C0GjV9n_7T5Rbw")
