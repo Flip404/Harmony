c3078799eafb86d39dcf4a73054116824a71fa4f
Flip <aungkhantpaing6789@gmail.com>
2021-10-18 20:37:46 +0630

    change music.py

--- music.py
+++ music.py
@@ -1,13 +1,14 @@
 import discord
 from discord.ext import commands
 import youtube_dl
+import youtubesearchpython as ysp
 import asyncio
 
 user = ''
 url_list = []
 title_list = {}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+YDL_OPTIONS = {'format': 'bestaudio'}
 
 
 class music(commands.Cog):
@@ -38,12 +39,14 @@ class music(commands.Cog):
         try:
             num = await self.check_voice_channel(ctx)
             if num:
+                new_url = ysp.VideosSearch(url, limit=1)
+                link = new_url.result()['result'][0]['link']
+                title = new_url.result()['result'][0]['title']
+                aa = [title, link]
                 with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-                    info = ydl.extract_info("ytsearch:%s" % url, download=False)['entries'][0]
+                    info = ydl.extract_info(link, download=False)
                     url2 = info['formats'][0]['url']
-                    title = info['title']
-                    source = discord.FFmpegPCMAudio(url2, **FFMPEG_OPTIONS)
-                    aa = [title, url2]
+                    source = discord.FFmpegOpusAudio(url2, **FFMPEG_OPTIONS)
                     url_list.append(source)
                     title_list[source] = aa
                 vc = ctx.voice_client
@@ -53,7 +56,7 @@ class music(commands.Cog):
                     vc.play(source, after=lambda x=None: self.check_queue(ctx))
                     vc.is_playing()
         except Exception as e:
-            await ctx.send(e)
+            await ctx.send(e, delete_after=5)
 
     def check_queue(self, ctx):
         if url_list:
