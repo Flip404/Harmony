b5ba9367ad6f5ae7a4413863f95c58d111dde2be
Flip <aungkhantpaing6789@gmail.com>
2021-11-13 23:42:48 +0630

    change the discord token to secure.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from discord.ext import commands
 import music
+import os
+import json
 
+if os.path.exists(os.getcwd() + "/config.json"):
+
+    with open("./config.json") as f:
+        configData = json.load(f)
+
+else:
+    configTemplate = {"Token": ""}
+
+    with open(os.getcwd() + "/config.json", "w+") as f:
+        json.dump(configTemplate, f)
+
+token = configData["Token"]
 cogs = [music]
 client = commands.Bot(command_prefix='\\')
 client.remove_command('help')
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run("ODk3ODQ5OTM0NDk2Mjc2NTAx.YWbp6Q.RSS69jEftslD1C0GjV9n_7T5Rbw")
+client.run(token)
